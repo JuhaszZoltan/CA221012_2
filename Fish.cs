3ad6f59b7d8e43d069437c5a02b78283902f589b
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2022-10-12 19:30:57 +0200

    Add project files.

---
+++ Fish.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CA221012_2
+{
+    internal class Fish
+    {
+        private float weight;
+        private bool isWeightSet = false;
+        private int swimTop;
+        private int swimDepth;
+        private string species;
+
+        public float Weight
+        {
+            get => weight;
+            set
+            {
+                if (value < .5f || value > 40f)
+                    throw new Exception("Egy hal súlya csak [0.5, 40] között valid");
+                if (isWeightSet)
+                {
+                    if (weight * 1.1f < value)
+                        throw new Exception("Egy hal súlya egyszerre ennyit nem nőhet");
+                    if (weight * .9f > value)
+                        throw new Exception("Egy hal súlya egyszerre ennyit nem csökkenhet");
+                }
+                weight = value;
+            }
+        }
+        public bool Predator { get; private set; }
+        public int SwimTop
+        {
+            get => swimTop;
+            set
+            {
+                if (value < 0 || value > 400)
+                    throw new Exception("Egy hal minimális úszási mélysége csak [0, 400] között valid");
+                swimTop = value;
+            }
+        }
+        public int SwimDepth
+        {
+            get => swimDepth;
+            set
+            {
+                if (value < 10 || value > 400)
+                    throw new Exception("Egy hal merülési sávjának szélessége csak [10, 400] között valid");
+                swimDepth = value;
+            }
+        }
+        public int SwimBtm => SwimTop + SwimDepth;
+        public string Species
+        {
+            get => species;
+            set
+            {
+                if (value is null)
+                    throw new Exception("Egy halnak a fajtája nem lehet null");
+                if (value.Length < 3 || value.Length > 30)
+                    throw new Exception("Egy halvajta minimum 3, maximum 30 karakter hosszó lehet");
+
+                species = value;
+            }
+        }
+
+        public void GetFishInfo()
+        {
+            Console.Write($"{Species, -8} ");
+            Console.ForegroundColor = Predator ? ConsoleColor.Red : ConsoleColor.Green;
+            Console.Write(Predator ? "(R) " : "(N) ");
+            Console.ResetColor();
+            Console.WriteLine($"{Weight,5:0.00}Kg " +
+                $"[{SwimTop,3}-{SwimBtm,3}]cm");
+        }
+
+        public Fish(float weight, bool predator, int swimTop, int swimDepth, string species)
+        {
+            Weight = weight;
+            isWeightSet = true;
+            Predator = predator;
+            SwimTop = swimTop;
+            SwimDepth = swimDepth;
+            Species = species;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using CA221012_2;
+
+Random rnd = new();
+List<Fish> fishList = new();
+string[] fishes = { "ponty", "harcsa", "keszeg", "kárász", "aranyhal", "busa", };
+
+InitRndFishList(100);
+// foreach (var fish in fishList) fish.GetFishInfo();
+Simulation(100);
+Console.WriteLine($"hal maradt: {fishList.Count}");
+
+void Simulation(int round)
+{
+	for (int i = 0; i < round; i++)
+	{
+		int x, y;
+		do
+		{
+			x = rnd.Next(fishList.Count);
+			y = rnd.Next(fishList.Count);
+		} while (x == y);
+
+		if (fishList[x].Predator != fishList[y].Predator)
+		{
+			Fish r, n;
+
+			if (fishList[x].Predator)
+			{
+				r = fishList[x];
+				n = fishList[y];
+			}
+			else
+			{
+                r = fishList[y];
+                n = fishList[x];
+            }
+
+			if (r.SwimTop <= n.SwimBtm && n.SwimTop <= r.SwimBtm)
+			{
+				if (rnd.Next(100) < 30)
+				{
+					fishList.Remove(n);
+					if (r.Weight * 1.1f <= 40f)
+						r.Weight *= 1.1f;
+					else r.Weight = 40f;
+
+					r.GetFishInfo();
+					Console.WriteLine("megeszi: ");
+					n.GetFishInfo();
+					Console.WriteLine("---------");
+				}
+				else
+				{
+					r.GetFishInfo();
+					Console.WriteLine("nem akarja megenni:");
+					n.GetFishInfo();
+                    Console.WriteLine("---------");
+                }
+			}
+			else
+			{
+                r.GetFishInfo();
+                Console.WriteLine("nem éri el:");
+                n.GetFishInfo();
+                Console.WriteLine("---------");
+            }
+
+        }
+	}
+}
+
+void InitRndFishList(int pieces)
+{
+	for (int i = 0; i < pieces; i++)
+	{
+		fishList.Add(new Fish(
+			weight: rnd.Next(1, 81) / 2f,
+			predator: rnd.Next(100) < 10,
+			swimTop: rnd.Next(401),
+			swimDepth: rnd.Next(10, 401),
+			species: fishes[rnd.Next(fishes.Length)]));
+	}
+}
